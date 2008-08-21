31972f4c7ad8e70bd92588e6ebe35fcab1c73373
retrakker <retrakker@d65a7696-613b-0410-ac10-aa9ec39ffaa2>
2008-08-21 03:48:04 +0000

    - added utilities - general cleanup of the buildsystem - added documentation to the packaging - added pattern files to the packaging
    git-svn-id: https://artoolkit.svn.sourceforge.net/svnroot/artoolkit/branches/hartmut-pre-2_8/artoolkit@291 d65a7696-613b-0410-ac10-aa9ec39ffaa2

--- CMakeModules/ARToolKitHelper.cmake
+++ CMakeModules/ARToolKitHelper.cmake
@@ -1,21 +1,22 @@
 # some simple macros to inject files into bundles
 
-MACRO(ARTOOLKIT_EXECUTABLE EXE_NAME SRCS)
-	FILE(GLOB _datafiles "${CMAKE_SOURCE_DIR}/bin/Data/*.dat")
-	FILE(GLOB _datafiles ${_datafiles} "${CMAKE_SOURCE_DIR}/bin/Data/*.xml")
-	FILE(GLOB _datafiles ${_datafiles} "${CMAKE_SOURCE_DIR}/bin/Data/patt.*")
-	IF(APPLE)
-		SET_SOURCE_FILES_PROPERTIES(
+macro(artoolkit_executable EXE_NAME SRCS)
+	file(GLOB _datafiles "${CMAKE_SOURCE_DIR}/bin/Data/*.dat")
+	file(GLOB _datafiles ${_datafiles} "${CMAKE_SOURCE_DIR}/bin/Data/*.xml")
+	file(GLOB _datafiles ${_datafiles} "${CMAKE_SOURCE_DIR}/bin/Data/patt.*")
+		
+	if(APPLE)
+		set_source_files_properties(
 			${_datafiles}
 			PROPERTIES
 			MACOSX_PACKAGE_LOCATION "Resources/Data"
 			)
 			
-		ADD_EXECUTABLE(${EXE_NAME} MACOSX_BUNDLE ${SRCS} ${_datafiles})
-	ELSE(APPLE)
-		ADD_EXECUTABLE(${EXE_NAME} ${SRCS} ${_datafiles})
-	ENDIF(APPLE)
-ENDMACRO(ARTOOLKIT_EXECUTABLE)
+		add_executable(${EXE_NAME} MACOSX_BUNDLE ${${SRCS}} ${_datafiles})
+	else(APPLE)
+		add_executable(${EXE_NAME} ${${SRCS}} ${_datafiles})
+	endif(APPLE)
+endmacro(artoolkit_executable)
 
 macro(artoolkit_lib_install target)
 
@@ -53,3 +54,84 @@ macro(artoolkit_exe_install target)
 		
 endmacro(artoolkit_exe_install target)
 
+
+macro(artoolkit_example_lite name source_files)
+
+	set(exe_name ${name})
+	
+	include_directories(${OPENGL_INCLUDE_DIR} ${GLUT_INCLUDE_DIR})
+
+	artoolkit_executable(${exe_name} ${source_files})
+
+	target_link_libraries(${exe_name} 
+		AR 
+		ARvideo
+		ARMulti
+		ARgsubUtil
+		ARgsub_lite
+		${OPENGL_LIBRARIES} ${GLUT_LIBRARIES}
+		)
+		
+	set_target_properties(${exe_name}
+		PROPERTIES
+		PROJECT_LABEL "Example ${name}"
+	)
+
+	artoolkit_exe_install(${exe_name})	
+
+endmacro(artoolkit_example_lite name source_files)
+
+
+macro(artoolkit_example_glut name source_files)
+
+	set(exe_name ${name})
+	
+	include_directories(${OPENGL_INCLUDE_DIR} ${GLUT_INCLUDE_DIR})
+	
+	artoolkit_executable(${exe_name} ${source_files})
+
+	target_link_libraries(${exe_name} 
+		AR 		
+		ARvideo
+		ARgsub
+		ARgsubUtil		
+		ARMulti
+		${OPENGL_LIBRARIES} ${GLUT_LIBRARIES}
+		)
+		
+	set_target_properties(${exe_name}
+		PROPERTIES
+		PROJECT_LABEL "Example ${name}"
+	)
+
+	artoolkit_exe_install(${exe_name})	
+
+endmacro(artoolkit_example_glut name source_files)
+
+
+macro(artoolkit_utility_glut name source_files)
+
+	set(exe_name ${name})
+	
+	include_directories(${OPENGL_INCLUDE_DIR} ${GLUT_INCLUDE_DIR})
+	
+	artoolkit_executable(${exe_name} ${source_files})
+
+	target_link_libraries(${exe_name} 
+		AR 
+		ARvideo
+		ARMulti		
+		ARgsub
+		ARgsub_lite
+		ARgsubUtil
+		${OPENGL_LIBRARIES} ${GLUT_LIBRARIES}
+		)
+		
+	set_target_properties(${exe_name}
+		PROPERTIES
+		PROJECT_LABEL "Utility ${name}"
+	)
+
+	artoolkit_exe_install(${exe_name})	
+
+endmacro(artoolkit_utility_glut name source_files)
